c1645de9493e00b9aaffd7ff00fb5b4057fa43e5
Jeff L <jtl2@illinois.edu>
2016-10-23 16:02:09 -0500

    creates authorizeUser function

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,7 +1,12 @@
 var User = require('/models/user.js');
+var spotifyApi = new SpotifyWebApi({
+  clientId : 'c0be0c89a1e241898635418ad5fbbbef',
+  clientSecret : '5adebeaaee924c3cad12ed37545a8489',
+  redirectUri : 'http://localhost:8080/callback'
+});
 
 module.exports = {
-  authorize: authorize
+  authorizeUser : authorizeUser
 };
 
 /*
@@ -11,7 +16,60 @@ module.exports = {
   Param 2: a handle to the response object
  */
 
-function authorize(req, res) {
-  var spotifytoken = req.swagger.params.spotifytoken.value;
+function authorizeUser(req, res) {
+  var spotifyCode = req.swagger.params.spotifyCode.value;
+  spotifyApi.authorizationCodeGrant(spotifyCode);
+  spotifyApi.getMe()
+  .then(function(data) {
+    var name = data.body.display_name;
+    var id = data.body.id;
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+  User.find({spotifyID:id}, function(err, sUser) {
+      if (err)
+          res.send(err);
+      if (sUser === null){
+          var user = new User();
+          user.spotifyID = id;
+          user.name = name;
+          user.friends = [];
+          user.save(function(err) {
+              if (err)
+                  res.send(err);
+
+              res.send('New Chordial User Created?!');
+          });
+          res.send(user.id);
+        }
+      else {
+        res.send(sUser.id);
+      }
+  });
   res.send(200);
+}
+
+function detailMe(req,res){
+  spotifyApi.getMe()
+  .then(function(data) {
+    console.log('Some information about the authenticated user', data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+}
+
+function detailFriends(req,res){
+
+}
+
+function addFriend(req,res){
+
+}
+
+function myMusic(req,res){
+
+}
+
+function shareCommon(req,res){
+
 }
