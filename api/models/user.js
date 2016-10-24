e0e8535f54443b03be18a628b3484cf87a1b4da9
Jeff L <jtl2@illinois.edu>
2016-10-24 16:25:27 -0500

    clears up api specifications

--- api/models/user.js
+++ api/models/user.js
@@ -6,7 +6,13 @@ var UserSchema = new Schema({
   name : String,
   spotifyID : String,
   chordialID : String,
-  friends : [{type : String}]
+  friends : [{type : String}],
+  tracks : [{type : String}]
+});
+
+var TrackSchema = new Schema({
+  trackName : String,
+  trackID : String
 });
 
 module.exports = mongoose.model('User',UserSchema);
